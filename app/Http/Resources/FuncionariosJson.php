af25e7eb42eef8d7a39a8ab724d5cc6925a6694d
Lucas Fernandes <lucashfernandes@yahoo.com.br>
2019-02-01 15:58:13 -0300

    Alteração FuncionariosJson

--- app/Http/Resources/FuncionariosJson.php
+++ app/Http/Resources/FuncionariosJson.php
@@ -14,6 +14,11 @@ class FuncionariosJson extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            "id"         => $this->id,
+            "Projeto_id" => $this->projeto_id,
+            "Gerente_id" => $this->gerente_id,
+            
+        ];
     }
 }
