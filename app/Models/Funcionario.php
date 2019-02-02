d70c937397f945cbc5da3a6bf8b2f272784961e0
Lucas Fernandes <lucashfernandes@yahoo.com.br>
2019-02-02 17:56:01 -0300

    Alteração na Model Funcionario

--- app/Models/Funcionario.php
+++ app/Models/Funcionario.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Funcionario extends Model
 {
-    protected $table = 'funcionarios';
-
     protected $fillable = [
     		'nome',
     		'projeto_id',
